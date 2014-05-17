db2649774dd92bf05086224f44a199fb1ba319dd
ferllini13 <ferllini_13@hotmail.com>
2014-05-17 14:06:46 -0600

    agredada clase de los botones  y mosica de seleccion

--- PyWars.py
+++ PyWars.py
@@ -1,13 +1,17 @@
 import pygame
 import sys
 from pygame.locals import *
+
 pygame.mixer.init()
-
-
-
 pygame.mixer.music.load("sound.mp3")
+select_sound= pygame.mixer.Sound("select.wav")
 
-
+#se cra la clase cursor, que es un rectangulo qeu sigue al mouse
+class cursor(pygame.Rect):
+    def __init__(self):
+        pygame.Rect.__init__(self,0,0,1,1)#cursos inicia en coordenadas 0,0 y es de 1x1
+    def update(self):#actualiza la pocision del cursos dependiendo de la del mouse
+        self.left,self.top=pygame.mouse.get_pos()#capta la posicion del mouse
 
 #se crea la clase para los button para los botones
 class button(pygame.sprite.Sprite):#se crea la clase para los botones
@@ -20,6 +24,7 @@ class button(pygame.sprite.Sprite):#se crea la clase para los botones
         
     def update(self,screen,cursor):#se actializa el boton
         if cursor.colliderect(self.rect):#se define la condicion cuando el cursor se pocisione sobre el boton
+            select_sound.play
             self.basic_pic=self.selected_pic
         else: self.basic_pic=self.unselected_pic #condicion del boton en stand by
         screen.blit(self.basic_pic,self.rect)#que se actualiza la pantalla dependiendo de la accion condicional
@@ -31,22 +36,18 @@ def main():
     screen=pygame.display.set_mode([1000,720])
     pygame.display.set_caption("Tarea Corta")
     wallp=pygame.image.load("wall2.jpg").convert()
+    cursor1=cursor()
     pygame.mixer.music.play() 
     
     
     while True:
         screen.blit(wallp,(0,0))
+        cursor1.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit(0)
-
-                
+                sys.exit(0)         
         pygame.display.flip()
-        
-        
-        
-    
     
     
 main()
